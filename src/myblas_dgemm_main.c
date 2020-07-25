346af2627ef7e83dd487240ec40ee1a0c5a7b612
J.Yamamoto <J.Yamamoto.Tech@gmail.com>
2020-07-25 21:45:31 +0900

    Changed strip-mining loops to blocking loops.

--- src/myblas_dgemm_main.c
+++ src/myblas_dgemm_main.c
@@ -97,32 +97,92 @@ void myblas_dgemm_main( gemm_args_t* args ){
 
 	    }else{
 
-	        for( size_t j3=0 ; j3<N; j3+=MIN(N-j3,MYBLAS_PANEL_N) ){
-	        for( size_t j2=j3; j2<MIN(j3+MYBLAS_PANEL_N,N); j2+=MIN(N-j2,MYBLAS_BLOCK_N) ){
-	        for( size_t j1=j2; j1<MIN(j2+MYBLAS_BLOCK_N,N); j1+=MIN(N-j1,MYBLAS_TILE_N ) ){
-	        for( size_t j =j1; j <MIN(j1+MYBLAS_TILE_N ,N); j++ ){
-	            for( size_t i3=0 ; i3<M; i3+=MIN(M-i3,MYBLAS_PANEL_M) ){
-	            for( size_t i2=i3; i2<MIN(i3+MYBLAS_PANEL_M,M); i2+=MIN(M-i2,MYBLAS_BLOCK_M) ){
-	            for( size_t i1=i2; i1<MIN(i2+MYBLAS_BLOCK_M,M); i1+=MIN(M-i1,MYBLAS_TILE_M ) ){
-	            for( size_t i =i1; i <MIN(i1+MYBLAS_TILE_M ,M); i++ ){
-			AB=0e0;
-	                for( size_t k3=0 ; k3<K; k3+=MIN(K-k3,MYBLAS_PANEL_K) ){
-	                for( size_t k2=k3; k2<MIN(k3+MYBLAS_PANEL_K,K); k2+=MIN(K-k2,MYBLAS_BLOCK_K) ){
-	                for( size_t k1=k2; k1<MIN(k2+MYBLAS_BLOCK_K,K); k1+=MIN(K-k1,MYBLAS_TILE_K ) ){
-	                for( size_t k =k1; k <MIN(k1+MYBLAS_TILE_K ,K); k++ ){
-	                   AB = AB + (*A)*(*B);
-	                   A += lda;
-	                   B++;
-	                }}}}
-			*C=beta*(*C) + alpha*AB;
-	                A = A - lda*K + 1;
-	                B = B - K;
+	        // scaling beta*C
+	        for( size_t j=0; j<N; j++ ){
+	            for( size_t i=0; i<M; i++ ){
+			*C=beta*(*C);
 	                C++;
-	            }}}}
-	            A = A - M;
-	            B = B + ldb;
+	            }
 	            C = C - M + ldc;
-	        }}}}
+	        }
+	        C = C - ldc*N; // retern to head of pointer.
+
+	        //for( size_t j3=0 ; j3<N; j3+=MIN(N-j3,MYBLAS_PANEL_N) ){
+	        //for( size_t j2=j3; j2<MIN(j3+MYBLAS_PANEL_N,N); j2+=MIN(N-j2,MYBLAS_BLOCK_N) ){
+	        //for( size_t j1=j2; j1<MIN(j2+MYBLAS_BLOCK_N,N); j1+=MIN(N-j1,MYBLAS_TILE_N ) ){
+	        //for( size_t j =j1; j <MIN(j1+MYBLAS_TILE_N ,N); j++ ){
+	        //    for( size_t i3=0 ; i3<M; i3+=MIN(M-i3,MYBLAS_PANEL_M) ){
+	        //    for( size_t i2=i3; i2<MIN(i3+MYBLAS_PANEL_M,M); i2+=MIN(M-i2,MYBLAS_BLOCK_M) ){
+	        //    for( size_t i1=i2; i1<MIN(i2+MYBLAS_BLOCK_M,M); i1+=MIN(M-i1,MYBLAS_TILE_M ) ){
+	        //    for( size_t i =i1; i <MIN(i1+MYBLAS_TILE_M ,M); i++ ){
+		//	AB=0e0;
+	        //        for( size_t k3=0 ; k3<K; k3+=MIN(K-k3,MYBLAS_PANEL_K) ){
+	        //        for( size_t k2=k3; k2<MIN(k3+MYBLAS_PANEL_K,K); k2+=MIN(K-k2,MYBLAS_BLOCK_K) ){
+	        //        for( size_t k1=k2; k1<MIN(k2+MYBLAS_BLOCK_K,K); k1+=MIN(K-k1,MYBLAS_TILE_K ) ){
+	        //        for( size_t k =k1; k <MIN(k1+MYBLAS_TILE_K ,K); k++ ){
+	        //           AB = AB + (*A)*(*B);
+	        //           A += lda;
+	        //           B++;
+	        //        }}}}
+		//	*C=(*C) + alpha*AB;
+	        //        A = A - lda*K + 1;
+	        //        B = B - K;
+	        //        C++;
+	        //    }}}}
+	        //    A = A - M;
+	        //    B = B + ldb;
+	        //    C = C - M + ldc;
+	        //}}}}
+
+	        // L3 cache
+	        for( size_t j3=0 ; j3<N; j3+=MIN(N-j3,MYBLAS_PANEL_N) ){
+	        for( size_t i3=0 ; i3<M; i3+=MIN(M-i3,MYBLAS_PANEL_M) ){
+	        for( size_t k3=0 ; k3<K; k3+=MIN(K-k3,MYBLAS_PANEL_K) ){
+	            // L2 cache
+	            for( size_t j2=j3; j2<MIN(j3+MYBLAS_PANEL_N,N); j2+=MIN(N-j2,MYBLAS_BLOCK_N) ){
+	            for( size_t i2=i3; i2<MIN(i3+MYBLAS_PANEL_M,M); i2+=MIN(M-i2,MYBLAS_BLOCK_M) ){
+	            for( size_t k2=k3; k2<MIN(k3+MYBLAS_PANEL_K,K); k2+=MIN(K-k2,MYBLAS_BLOCK_K) ){
+	                // L1 cache
+	                for( size_t j1=j2; j1<MIN(j2+MYBLAS_BLOCK_N,N); j1+=MIN(N-j1,MYBLAS_TILE_N ) ){
+	                for( size_t i1=i2; i1<MIN(i2+MYBLAS_BLOCK_M,M); i1+=MIN(M-i1,MYBLAS_TILE_M ) ){
+	                for( size_t k1=k2; k1<MIN(k2+MYBLAS_BLOCK_K,K); k1+=MIN(K-k1,MYBLAS_TILE_K ) ){
+
+	                    A = A + lda*k1 + i1;
+	                    B = B + ldb*j1 + k1;
+	                    C = C + ldc*j1 + i1;
+
+	                    size_t M1 = MIN(MYBLAS_TILE_M ,M-i1);
+	                    size_t N1 = MIN(MYBLAS_TILE_N ,N-j1);
+	                    size_t K1 = MIN(MYBLAS_TILE_K ,K-k1);
+	                    for( size_t j =j1; j < j1+N1; j++ ){
+	                      for( size_t i =i1; i < i1+M1; i++ ){
+		                AB=0e0;
+	                        for( size_t k =k1; k < k1+K1; k++ ){
+	                          AB = AB + (*A)*(*B);
+	                          A += lda;
+	                          B++;
+	                        }
+			        *C = (*C) + alpha*AB;
+	                        A = A - lda*K1 + 1;
+	                        B = B - K1;
+	                        C++;
+	                      }
+	                      A = A - M1;
+	                      B = B + ldb;
+	                      C = C - M1 + ldc;
+	                    }
+	                    B = B - ldb*N1;
+	                    C = C - ldc*N1;
+
+	                    A = A - lda*k1 - i1;
+	                    B = B - ldb*j1 - k1;
+	                    C = C - ldc*j1 - i1;
+
+	                }}}
+
+	            }}}
+
+	        }}}
 
 	    }
 	}
