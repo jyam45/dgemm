be54376d3167d78b7015d3cc17b98d6af8af0116
J.Yamamoto <J.Yamamoto.Tech@gmail.com>
2020-07-28 23:05:18 +0900

    Loop Exchange for L2 blocking loop

--- src/myblas_dgemm_main.c
+++ src/myblas_dgemm_main.c
@@ -104,9 +104,7 @@ void myblas_dgemm_main( gemm_args_t* args ){
 	        myblas_dgemm_scale2d(beta,C,ldc,&infoC);
 
 		double*   A2 = calloc( MYBLAS_BLOCK_M*MYBLAS_BLOCK_K, sizeof(double) );
-	        size_t  lda2 = MYBLAS_BLOCK_M;
 		double*   B2 = calloc( MYBLAS_BLOCK_K*MYBLAS_BLOCK_N, sizeof(double) );
-	        size_t  ldb2 = MYBLAS_BLOCK_K;
 
 	        // L3 cache
 	        for( size_t j3=0 ; j3<N; j3+=MIN(N-j3,MYBLAS_PANEL_N) ){
@@ -115,27 +113,32 @@ void myblas_dgemm_main( gemm_args_t* args ){
 	            size_t M3 = MIN(MYBLAS_PANEL_M ,M-i3);
 	            size_t N3 = MIN(MYBLAS_PANEL_N ,N-j3);
 	            size_t K3 = MIN(MYBLAS_PANEL_K ,K-k3);
+
 	            // L2 cache
-	            for( size_t j2=j3; j2<j3+N3; j2+=MIN(N-j2,MYBLAS_BLOCK_N) ){
-	            for( size_t i2=i3; i2<i3+M3; i2+=MIN(M-i2,MYBLAS_BLOCK_M) ){
 	            for( size_t k2=k3; k2<k3+K3; k2+=MIN(K-k2,MYBLAS_BLOCK_K) ){
+	              size_t K2 = MIN(MYBLAS_BLOCK_K ,K-k2);
+
+	              for( size_t i2=i3; i2<i3+M3; i2+=MIN(M-i2,MYBLAS_BLOCK_M) ){
 	                size_t M2 = MIN(MYBLAS_BLOCK_M ,M-i2);
-	                size_t N2 = MIN(MYBLAS_BLOCK_N ,N-j2);
-	                size_t K2 = MIN(MYBLAS_BLOCK_K ,K-k2);
 
 	                // On L2-Cache Copy for A
 	                block2d_info_t infoA = {M2,K2,MYBLAS_TILE_M,MYBLAS_TILE_K};
 	                myblas_dgemm_copy_t(A+lda*k2+i2,lda,A2,&infoA);
 
-	                // On L2-Cache Copy for B
-	                block2d_info_t infoB = {K2,N2,MYBLAS_TILE_K,MYBLAS_TILE_N};
-	                myblas_dgemm_copy_n(B+ldb*j2+k2,ldb,B2,&infoB);
+	                for( size_t j2=j3; j2<j3+N3; j2+=MIN(N-j2,MYBLAS_BLOCK_N) ){
+	                  size_t N2 = MIN(MYBLAS_BLOCK_N ,N-j2);
 
-	                // L1 cache
-	                block3d_info_t info3d = {M2,N2,K2,MYBLAS_TILE_M,MYBLAS_TILE_N,MYBLAS_TILE_K};
-	                myblas_dgemm_kernel(alpha,A2,B2,C+ldc*j2+i2,ldc,&info3d);
+	                  // On L2-Cache Copy for B
+	                  block2d_info_t infoB = {K2,N2,MYBLAS_TILE_K,MYBLAS_TILE_N};
+	                  myblas_dgemm_copy_n(B+ldb*j2+k2,ldb,B2,&infoB);
 
-	            }}}
+	                  // L1 cache
+	                  block3d_info_t info3d = {M2,N2,K2,MYBLAS_TILE_M,MYBLAS_TILE_N,MYBLAS_TILE_K};
+	                  myblas_dgemm_kernel(alpha,A2,B2,C+ldc*j2+i2,ldc,&info3d);
+
+	                }
+	              }
+	            }
 
 	        }}}
 
