764dbbd3d11b5b7f5751045ff5fe724b72b8a36e
J.Yamamoto <J.Yamamoto.Tech@gmail.com>
2020-08-06 15:48:19 +0900

    Re-unroll the kernel and copy functions with 4x4x4 unroll distance.

--- src/myblas_dgemm_copy_n.c
+++ src/myblas_dgemm_copy_n.c
@@ -61,106 +61,37 @@ void myblas_dgemm_copy_n_core(const double* B, size_t ldb, double* B2, const blo
 	size_t n = N1;
 	if( n >> 2 ){
 	  size_t n4 = ( n >> 2 ); // unrolling with 8 elements
-	  while( n4-- )
-	  {
+	  while( n4-- ) {
 	    size_t k = K1;
-	    //if( k >> 4 ){
-	    //  size_t k16 = ( k >> 4 ); // unrolling with 16 elements
-	    //  while( k16-- ){
-	    //    size_t k8a = 4; // Vector-lenght of YMM
-	    //    size_t k8b = 4; // Vector-lenght of YMM
-	    //    size_t k8c = 4; // Vector-lenght of YMM
-	    //    size_t k8d = 4; // Vector-lenght of YMM
-	    //    while( k8a-- ){
-	    //      ymm0  = *(B +0*ldb);
-	    //      ymm1  = *(B +1*ldb);
-	    //      ymm2  = *(B +2*ldb);
-	    //      ymm3  = *(B +3*ldb);
-	    //      *(B2+0*K1) = ymm0 ;
-	    //      *(B2+1*K1) = ymm1 ;
-	    //      *(B2+2*K1) = ymm2 ;
-	    //      *(B2+3*K1) = ymm3 ;
-	    //      B++;
-	    //      B2++;
-	    //    }
-	    //    while( k8b-- ){
-	    //      ymm4  = *(B +0*ldb);
-	    //      ymm5  = *(B +1*ldb);
-	    //      ymm6  = *(B +2*ldb);
-	    //      ymm7  = *(B +3*ldb);
-	    //      *(B2+0*K1) = ymm4 ;
-	    //      *(B2+1*K1) = ymm5 ;
-	    //      *(B2+2*K1) = ymm6 ;
-	    //      *(B2+3*K1) = ymm7 ;
-	    //      B++;
-	    //      B2++;
-	    //    }
-	    //    while( k8c-- ){
-	    //      ymm8  = *(B +0*ldb);
-	    //      ymm9  = *(B +1*ldb);
-	    //      ymm10 = *(B +2*ldb);
-	    //      ymm11 = *(B +3*ldb);
-	    //      *(B2+0*K1) = ymm8 ;
-	    //      *(B2+1*K1) = ymm9 ;
-	    //      *(B2+2*K1) = ymm10;
-	    //      *(B2+3*K1) = ymm11;
-	    //      B++;
-	    //      B2++;
-	    //    }
-	    //    while( k8d-- ){
-	    //      ymm12 = *(B +0*ldb);
-	    //      ymm13 = *(B +1*ldb);
-	    //      ymm14 = *(B +2*ldb);
-	    //      ymm15 = *(B +3*ldb);
-	    //      *(B2+0*K1) = ymm12;
-	    //      *(B2+1*K1) = ymm13;
-	    //      *(B2+2*K1) = ymm14;
-	    //      *(B2+3*K1) = ymm15;
-	    //      B++;
-	    //      B2++;
-	    //    }
-
-	    //  }
-	    //}
-	    ////if( k >> 3 ){
-	    ////  size_t k8 = ( k >> 3 ); // unrolling with 8 elements
-	    ////  while( k8-- ){
-	    //if( k & 8 ){
-	    //  {
-	    //    size_t k8a = 4; // Vector-lenght of YMM
-	    //    size_t k8b = 4; // Vector-lenght of YMM
-	    //    while( k8a-- ){
-	    //      ymm0  = *(B +0*ldb);
-	    //      ymm1  = *(B +1*ldb);
-	    //      ymm2  = *(B +2*ldb);
-	    //      ymm3  = *(B +3*ldb);
-	    //      *(B2+0*K1) = ymm0 ;
-	    //      *(B2+1*K1) = ymm1 ;
-	    //      *(B2+2*K1) = ymm2 ;
-	    //      *(B2+3*K1) = ymm3 ;
-	    //      B++;
-	    //      B2++;
-	    //    }
-	    //    while( k8b-- ){
-	    //      ymm4  = *(B +0*ldb);
-	    //      ymm5  = *(B +1*ldb);
-	    //      ymm6  = *(B +2*ldb);
-	    //      ymm7  = *(B +3*ldb);
-	    //      *(B2+0*K1) = ymm4 ;
-	    //      *(B2+1*K1) = ymm5 ;
-	    //      *(B2+2*K1) = ymm6 ;
-	    //      *(B2+3*K1) = ymm7 ;
-	    //      B++;
-	    //      B2++;
-	    //    }
-	    //  }
-	    //}
+/*
+	    if( k >> 3 ){
+	      size_t k8 = ( k >> 3 ); // unrolling with 8 elements
+	      while( k8-- ){
+	          ymm0  = *(B+0+0*ldb); ymm4  = *(B+1+0*ldb); ymm8  = *(B+2+0*ldb); ymm12 = *(B+3+0*ldb);
+	          ymm1  = *(B+0+1*ldb); ymm5  = *(B+1+1*ldb); ymm9  = *(B+2+1*ldb); ymm13 = *(B+3+1*ldb);
+	          ymm2  = *(B+0+2*ldb); ymm6  = *(B+1+2*ldb); ymm10 = *(B+2+2*ldb); ymm14 = *(B+3+2*ldb);
+	          ymm3  = *(B+0+3*ldb); ymm7  = *(B+1+3*ldb); ymm11 = *(B+2+3*ldb); ymm15 = *(B+3+3*ldb);
+	          *(B2+0+0*8) = ymm0 ; *(B2+1+0*8) = ymm4 ; *(B2+2+0*8) = ymm8 ; *(B2+3+0*8) = ymm12;
+	          *(B2+0+1*8) = ymm1 ; *(B2+1+1*8) = ymm5 ; *(B2+2+1*8) = ymm9 ; *(B2+3+1*8) = ymm13;
+	          *(B2+0+2*8) = ymm2 ; *(B2+1+2*8) = ymm6 ; *(B2+2+2*8) = ymm10; *(B2+3+2*8) = ymm14;
+	          *(B2+0+3*8) = ymm3 ; *(B2+1+3*8) = ymm7 ; *(B2+2+3*8) = ymm11; *(B2+3+3*8) = ymm15;
+	          ymm0  = *(B+4+0*ldb); ymm4  = *(B+5+0*ldb); ymm8  = *(B+6+0*ldb); ymm12 = *(B+7+0*ldb);
+	          ymm1  = *(B+4+1*ldb); ymm5  = *(B+5+1*ldb); ymm9  = *(B+6+1*ldb); ymm13 = *(B+7+1*ldb);
+	          ymm2  = *(B+4+2*ldb); ymm6  = *(B+5+2*ldb); ymm10 = *(B+6+2*ldb); ymm14 = *(B+7+2*ldb);
+	          ymm3  = *(B+4+3*ldb); ymm7  = *(B+5+3*ldb); ymm11 = *(B+6+3*ldb); ymm15 = *(B+7+3*ldb);
+	          *(B2+4+0*8) = ymm0 ; *(B2+5+0*8) = ymm4 ; *(B2+6+0*8) = ymm8 ; *(B2+7+0*8) = ymm12;
+	          *(B2+4+1*8) = ymm1 ; *(B2+5+1*8) = ymm5 ; *(B2+6+1*8) = ymm9 ; *(B2+7+1*8) = ymm13;
+	          *(B2+4+2*8) = ymm2 ; *(B2+5+2*8) = ymm6 ; *(B2+6+2*8) = ymm10; *(B2+7+2*8) = ymm14;
+	          *(B2+4+3*8) = ymm3 ; *(B2+5+3*8) = ymm7 ; *(B2+6+3*8) = ymm11; *(B2+7+3*8) = ymm15;
+	          B+=8;
+	          B2+=32;
+	      }
+	    }
+	    if( k & 4 ){
+*/
 	    if( k >> 2 ){
 	      size_t k4 = ( k >> 2 ); // unrolling with 8 elements
 	      while( k4-- ){
-	    //if( k & 4 ){
-	    //    size_t k8a = 4; // Vector-lenght of YMM
-	    //    while( k8a-- ){
 	          ymm0  = *(B+0+0*ldb); ymm4  = *(B+1+0*ldb); ymm8  = *(B+2+0*ldb); ymm12 = *(B+3+0*ldb);
 	          ymm1  = *(B+0+1*ldb); ymm5  = *(B+1+1*ldb); ymm9  = *(B+2+1*ldb); ymm13 = *(B+3+1*ldb);
 	          ymm2  = *(B+0+2*ldb); ymm6  = *(B+1+2*ldb); ymm10 = *(B+2+2*ldb); ymm14 = *(B+3+2*ldb);
@@ -171,144 +102,158 @@ void myblas_dgemm_copy_n_core(const double* B, size_t ldb, double* B2, const blo
 	          *(B2+0+3*4) = ymm3 ; *(B2+1+3*4) = ymm7 ; *(B2+2+3*4) = ymm11; *(B2+3+3*4) = ymm15;
 	          B+=4;
 	          B2+=16;
-	     //   }
 	      }
 	    }
-	    if( k & 3 ){
-	      size_t kr = ( k & 3 ); // unrolling with 8 elements
-	      while( kr-- ){
-	        ymm0  = *(B +0*ldb);
-	        ymm1  = *(B +1*ldb);
-	        ymm2  = *(B +2*ldb);
-	        ymm3  = *(B +3*ldb);
-	        *(B2+0) = ymm0 ;
-	        *(B2+1) = ymm1 ;
-	        *(B2+2) = ymm2 ;
-	        *(B2+3) = ymm3 ;
-	        B++;
+	    if( k & 2 ){
+	        ymm0  = *(B+0+0*ldb); ymm4  = *(B+1+0*ldb);
+	        ymm1  = *(B+0+1*ldb); ymm5  = *(B+1+1*ldb);
+	        ymm2  = *(B+0+2*ldb); ymm6  = *(B+1+2*ldb);
+	        ymm3  = *(B+0+3*ldb); ymm7  = *(B+1+3*ldb);
+	        *(B2+0+0*2) = ymm0 ; *(B2+1+0*2) = ymm4 ;
+	        *(B2+0+1*2) = ymm1 ; *(B2+1+1*2) = ymm5 ;
+	        *(B2+0+2*2) = ymm2 ; *(B2+1+2*2) = ymm6 ;
+	        *(B2+0+3*2) = ymm3 ; *(B2+1+3*2) = ymm7 ;
+	        B+=2;
+	        B2+=8;
+	    }
+	    if( k & 1 ){
+	        ymm0  = *(B+0+0*ldb); 
+	        ymm1  = *(B+0+1*ldb); 
+	        ymm2  = *(B+0+2*ldb); 
+	        ymm3  = *(B+0+3*ldb); 
+	        *(B2+0+0*1) = ymm0 ;
+	        *(B2+0+1*1) = ymm1 ;
+	        *(B2+0+2*1) = ymm2 ;
+	        *(B2+0+3*1) = ymm3 ;
+	        B+=1;
 	        B2+=4;
-	      }
 	    }
+	    //if( k & 3 ){
+	    //  size_t kr = ( k & 3 ); // unrolling with 8 elements
+	    //  while( kr-- ){
+	    //    ymm0  = *(B +0*ldb);
+	    //    ymm1  = *(B +1*ldb);
+	    //    ymm2  = *(B +2*ldb);
+	    //    ymm3  = *(B +3*ldb);
+	    //    *(B2+0) = ymm0 ;
+	    //    *(B2+1) = ymm1 ;
+	    //    *(B2+2) = ymm2 ;
+	    //    *(B2+3) = ymm3 ;
+	    //    B++;
+	    //    B2+=4;
+	    //  }
+	    //}
 	    B  = B  - K1 + 4*ldb ;
 	    //B2 = B2 - K1 + 4*K1;
 	  }
 	}
-	//if( n & 2 ){
-	////if( n >> 1 ){
-	////  size_t n2 = ( n >> 1 ); // unrolling with 8 elements
-	////  while( n2-- )
-	//  {
-	//    size_t k = K1;
-	//    if( k >> 4 ){
-	//      size_t k16 = ( k >> 4 ); // unrolling with 16 elements
-	//      while( k16-- ){
-	//        size_t k8a = 4; // Vector-lenght of YMM
-	//        size_t k8b = 4; // Vector-lenght of YMM
-	//        size_t k8c = 4; // Vector-lenght of YMM
-	//        size_t k8d = 4; // Vector-lenght of YMM
-	//        while( k8a-- ){
-	//          ymm0  = *(B +0*ldb);
-	//          ymm1  = *(B +1*ldb);
-	//          *(B2+0*K1) = ymm0 ;
-	//          *(B2+1*K1) = ymm1 ;
-	//          B++;
-	//          B2++;
-	//        }
-	//        while( k8b-- ){
-	//          ymm4  = *(B +0*ldb);
-	//          ymm5  = *(B +1*ldb);
-	//          *(B2+0*K1) = ymm4 ;
-	//          *(B2+1*K1) = ymm5 ;
-	//          B++;
-	//          B2++;
-	//        }
-	//        while( k8c-- ){
-	//          ymm8  = *(B +0*ldb);
-	//          ymm9  = *(B +1*ldb);
-	//          *(B2+0*K1) = ymm8 ;
-	//          *(B2+1*K1) = ymm9 ;
-	//          B++;
-	//          B2++;
-	//        }
-	//        while( k8d-- ){
-	//          ymm12 = *(B +0*ldb);
-	//          ymm13 = *(B +1*ldb);
-	//          *(B2+0*K1) = ymm12;
-	//          *(B2+1*K1) = ymm13;
-	//          B++;
-	//          B2++;
-	//        }
+	if( n & 2 ){
 
-	//      }
-	//    }
-	//    //if( k >> 3 ){
-	//    //  size_t k8 = ( k >> 3 ); // unrolling with 8 elements
-	//    //  while( k8-- ){
-	//    if( k & 8 ){
-	//      {
-	//        size_t k8a = 4; // Vector-lenght of YMM
-	//        size_t k8b = 4; // Vector-lenght of YMM
-	//        while( k8a-- ){
-	//          ymm0  = *(B +0*ldb);
-	//          ymm1  = *(B +1*ldb);
-	//          *(B2+0*K1) = ymm0 ;
-	//          *(B2+1*K1) = ymm1 ;
-	//          B++;
-	//          B2++;
-	//        }
-	//        while( k8b-- ){
-	//          ymm4  = *(B +0*ldb);
-	//          ymm5  = *(B +1*ldb);
-	//          *(B2+0*K1) = ymm4 ;
-	//          *(B2+1*K1) = ymm5 ;
-	//          B++;
-	//          B2++;
-	//        }
-	//      }
-	//    }
-	//    //if( k >> 2 ){
-	//    //  size_t k4 = ( k >> 2 ); // unrolling with 8 elements
-	//    //  while( k4-- ){
-	//    if( k & 4 ){
-	//        size_t k8a = 4; // Vector-lenght of YMM
-	//        while( k8a-- ){
-	//          ymm0  = *(B +0*ldb);
-	//          ymm1  = *(B +1*ldb);
-	//          *(B2+0*K1) = ymm0 ;
-	//          *(B2+1*K1) = ymm1 ;
-	//          B++;
-	//          B2++;
-	//        }
-	//    }
-	//    if( k & 3 ){
-	//      size_t kr = ( k & 3 ); // unrolling with 8 elements
-	//      while( kr-- ){
-	//        ymm0  = *(B +0*ldb);
-	//        ymm1  = *(B +1*ldb);
-	//        *(B2+0*K1) = ymm0 ;
-	//        *(B2+1*K1) = ymm1 ;
-	//        B++;
-	//        B2++;
-	//      }
-	//    }
-	//    B  = B  - K1 + 2*ldb ;
-	//    B2 = B2 - K1 + 2*K1;
-	//  }
-	//}
-
-	if( n & 3 ){
-	  size_t nr = ( n & 3 );
-	  while( nr-- ){
 	    size_t k = K1;
-	    while( k-- ){
-	      *B2=*B;
-	      B++;
-	      B2++;
+/*
+	    if( k >> 3 ){
+	      size_t k8 = ( k >> 3 ); // unrolling with 8 elements
+	      while( k8-- ){
+	          ymm0  = *(B+0+0*ldb); ymm4  = *(B+1+0*ldb); ymm8  = *(B+2+0*ldb); ymm12 = *(B+3+0*ldb);
+	          ymm1  = *(B+0+1*ldb); ymm5  = *(B+1+1*ldb); ymm9  = *(B+2+1*ldb); ymm13 = *(B+3+1*ldb);
+	          *(B2+0+0*8) = ymm0 ; *(B2+1+0*8) = ymm4 ; *(B2+2+0*8) = ymm8 ; *(B2+3+0*8) = ymm12;
+	          *(B2+0+1*8) = ymm1 ; *(B2+1+1*8) = ymm5 ; *(B2+2+1*8) = ymm9 ; *(B2+3+1*8) = ymm13;
+	          ymm0  = *(B+4+0*ldb); ymm4  = *(B+5+0*ldb); ymm8  = *(B+6+0*ldb); ymm12 = *(B+7+0*ldb);
+	          ymm1  = *(B+4+1*ldb); ymm5  = *(B+5+1*ldb); ymm9  = *(B+6+1*ldb); ymm13 = *(B+7+1*ldb);
+	          *(B2+4+0*8) = ymm0 ; *(B2+5+0*8) = ymm4 ; *(B2+6+0*8) = ymm8 ; *(B2+7+0*8) = ymm12;
+	          *(B2+4+1*8) = ymm1 ; *(B2+5+1*8) = ymm5 ; *(B2+6+1*8) = ymm9 ; *(B2+7+1*8) = ymm13;
+	          B+=8;
+	          B2+=16;
+	      }
 	    }
-	    B  = B  - K1 + ldb ;
-	  }
+	    if( k & 4 ){
+*/
+	    if( k >> 2 ){
+	      size_t k4 = ( k >> 2 ); // unrolling with 8 elements
+	      while( k4-- ){
+	          ymm0  = *(B+0+0*ldb); ymm4  = *(B+1+0*ldb); ymm8  = *(B+2+0*ldb); ymm12 = *(B+3+0*ldb);
+	          ymm1  = *(B+0+1*ldb); ymm5  = *(B+1+1*ldb); ymm9  = *(B+2+1*ldb); ymm13 = *(B+3+1*ldb);
+	          *(B2+0+0*4) = ymm0 ; *(B2+1+0*4) = ymm4 ; *(B2+2+0*4) = ymm8 ; *(B2+3+0*4) = ymm12;
+	          *(B2+0+1*4) = ymm1 ; *(B2+1+1*4) = ymm5 ; *(B2+2+1*4) = ymm9 ; *(B2+3+1*4) = ymm13;
+	          B+=4;
+	          B2+=8;
+	      }
+	    }
+	    if( k & 2 ){
+	        ymm0  = *(B+0+0*ldb); ymm4  = *(B+1+0*ldb);
+	        ymm1  = *(B+0+1*ldb); ymm5  = *(B+1+1*ldb);
+	        *(B2+0+0*2) = ymm0 ; *(B2+1+0*2) = ymm4 ;
+	        *(B2+0+1*2) = ymm1 ; *(B2+1+1*2) = ymm5 ;
+	        B+=2;
+	        B2+=4;
+	    }
+	    if( k & 1 ){
+	        ymm0  = *(B+0+0*ldb); 
+	        ymm1  = *(B+0+1*ldb); 
+	        *(B2+0+0*1) = ymm0 ;
+	        *(B2+0+1*1) = ymm1 ;
+	        B+=1;
+	        B2+=2;
+	    }
+	    B  = B  - K1 + 2*ldb ;
+	    //B2 = B2 - K1 + 4*K1;
 
 	}
+	if( n & 1 ){
+
+	    size_t k = K1;
+/*
+	    if( k >> 3 ){
+	      size_t k8 = ( k >> 3 ); // unrolling with 8 elements
+	      while( k8-- ){
+	          ymm0  = *(B+0+0*ldb); ymm4  = *(B+1+0*ldb); ymm8  = *(B+2+0*ldb); ymm12 = *(B+3+0*ldb);
+	          *(B2+0+0*8) = ymm0 ; *(B2+1+0*8) = ymm4 ; *(B2+2+0*8) = ymm8 ; *(B2+3+0*8) = ymm12;
+	          ymm0  = *(B+4+0*ldb); ymm4  = *(B+5+0*ldb); ymm8  = *(B+6+0*ldb); ymm12 = *(B+7+0*ldb);
+	          *(B2+4+0*8) = ymm0 ; *(B2+5+0*8) = ymm4 ; *(B2+6+0*8) = ymm8 ; *(B2+7+0*8) = ymm12;
+	          B+=8;
+	          B2+=8;
+	      }
+	    }
+	    if( k & 4 ){
+*/
+	    if( k >> 2 ){
+	      size_t k4 = ( k >> 2 ); // unrolling with 8 elements
+	      while( k4-- ){
+	          ymm0  = *(B+0+0*ldb); ymm4  = *(B+1+0*ldb); ymm8  = *(B+2+0*ldb); ymm12 = *(B+3+0*ldb);
+	          *(B2+0+0*4) = ymm0 ; *(B2+1+0*4) = ymm4 ; *(B2+2+0*4) = ymm8 ; *(B2+3+0*4) = ymm12;
+	          B+=4;
+	          B2+=4;
+	      }
+	    }
+	    if( k & 2 ){
+	        ymm0  = *(B+0+0*ldb); ymm4  = *(B+1+0*ldb);
+	        *(B2+0+0*2) = ymm0 ; *(B2+1+0*2) = ymm4 ;
+	        B+=2;
+	        B2+=2;
+	    }
+	    if( k & 1 ){
+	        ymm0  = *(B+0+0*ldb); 
+	        *(B2+0+0*1) = ymm0 ;
+	        B+=1;
+	        B2+=1;
+	    }
+	    B  = B  - K1 + 1*ldb ;
+	    //B2 = B2 - K1 + 4*K1;
+
+	}
+
+	//if( n & 3 ){
+	//  size_t nr = ( n & 3 );
+	//  while( nr-- ){
+	//    size_t k = K1;
+	//    while( k-- ){
+	//      *B2=*B;
+	//      B++;
+	//      B2++;
+	//    }
+	//    B  = B  - K1 + ldb ;
+	//  }
+	//}
 
 
 }
--- src/myblas_dgemm_copy_t.c
+++ src/myblas_dgemm_copy_t.c
@@ -60,71 +60,36 @@ void myblas_dgemm_copy_t_core(const double* A, size_t lda, double* A2, const blo
 	A = A + lda*k1 + i1; // start point
 
 	size_t m = M1;
-	//if( m >> 3 ){//1
-	//  size_t m8 = ( m >> 3 ); // unrolling
-	//  while( m8-- ){//2
-	//    size_t k = K1;
-
-	//    if( k >> 2 ){//3
-	//      size_t k4 = ( k >> 2 ); // unrolling
-	//      while( k4-- ){//4
-	//        ymm0  = *(A + 0 + 0*lda); ymm4  = *(A + 0 + 1*lda); ymm8  = *(A + 0 + 2*lda); ymm12 = *(A + 0 + 3*lda);
-	//        ymm1  = *(A + 1 + 0*lda); ymm5  = *(A + 1 + 1*lda); ymm9  = *(A + 1 + 2*lda); ymm13 = *(A + 1 + 3*lda);
-	//        ymm2  = *(A + 2 + 0*lda); ymm6  = *(A + 2 + 1*lda); ymm10 = *(A + 2 + 2*lda); ymm14 = *(A + 2 + 3*lda);
-	//        ymm3  = *(A + 3 + 0*lda); ymm7  = *(A + 3 + 1*lda); ymm11 = *(A + 3 + 2*lda); ymm15 = *(A + 3 + 3*lda);
-	//        *(A2 + 0 + 0*K1) = ymm0 ; *(A2 + 1 + 0*K1) = ymm4 ; *(A2 + 2 + 0*K1) = ymm8 ; *(A2 + 3 + 0*K1) = ymm12;
-	//        *(A2 + 0 + 1*K1) = ymm1 ; *(A2 + 1 + 1*K1) = ymm5 ; *(A2 + 2 + 1*K1) = ymm9 ; *(A2 + 3 + 1*K1) = ymm13;
-	//        *(A2 + 0 + 2*K1) = ymm2 ; *(A2 + 1 + 2*K1) = ymm6 ; *(A2 + 2 + 2*K1) = ymm10; *(A2 + 3 + 2*K1) = ymm14;
-	//        *(A2 + 0 + 3*K1) = ymm3 ; *(A2 + 1 + 3*K1) = ymm7 ; *(A2 + 2 + 3*K1) = ymm11; *(A2 + 3 + 3*K1) = ymm15;
-	//        ymm0  = *(A + 4 + 0*lda); ymm4  = *(A + 4 + 1*lda); ymm8  = *(A + 4 + 2*lda); ymm12 = *(A + 4 + 3*lda);
-	//        ymm1  = *(A + 5 + 0*lda); ymm5  = *(A + 5 + 1*lda); ymm9  = *(A + 5 + 2*lda); ymm13 = *(A + 5 + 3*lda);
-	//        ymm2  = *(A + 6 + 0*lda); ymm6  = *(A + 6 + 1*lda); ymm10 = *(A + 6 + 2*lda); ymm14 = *(A + 6 + 3*lda);
-	//        ymm3  = *(A + 7 + 0*lda); ymm7  = *(A + 7 + 1*lda); ymm11 = *(A + 7 + 2*lda); ymm15 = *(A + 7 + 3*lda);
-	//        *(A2 + 0 + 4*K1) = ymm0 ; *(A2 + 1 + 4*K1) = ymm4 ; *(A2 + 2 + 4*K1) = ymm8 ; *(A2 + 3 + 4*K1) = ymm12;
-	//        *(A2 + 0 + 5*K1) = ymm1 ; *(A2 + 1 + 5*K1) = ymm5 ; *(A2 + 2 + 5*K1) = ymm9 ; *(A2 + 3 + 5*K1) = ymm13;
-	//        *(A2 + 0 + 6*K1) = ymm2 ; *(A2 + 1 + 6*K1) = ymm6 ; *(A2 + 2 + 6*K1) = ymm10; *(A2 + 3 + 6*K1) = ymm14;
-	//        *(A2 + 0 + 7*K1) = ymm3 ; *(A2 + 1 + 7*K1) = ymm7 ; *(A2 + 2 + 7*K1) = ymm11; *(A2 + 3 + 7*K1) = ymm15;
-	//        A += 4*lda ;
-	//        A2+= 4;;
-	//      }//4
-	//    }//3
-	//    if( k & 3 ){//5
-	//      size_t kr = ( k & 3 ); 
-	//      while( kr-- ){//6
-	//        ymm0  = *(A + 0 + 0*lda);
-	//        ymm1  = *(A + 1 + 0*lda);
-	//        ymm2  = *(A + 2 + 0*lda);
-	//        ymm3  = *(A + 3 + 0*lda);
-	//        *(A2 + 0 + 0*K1) = ymm0 ;
-	//        *(A2 + 0 + 1*K1) = ymm1 ;
-	//        *(A2 + 0 + 2*K1) = ymm2 ;
-	//        *(A2 + 0 + 3*K1) = ymm3 ;
-	//        ymm0  = *(A + 4 + 0*lda);
-	//        ymm1  = *(A + 5 + 0*lda);
-	//        ymm2  = *(A + 6 + 0*lda);
-	//        ymm3  = *(A + 7 + 0*lda);
-	//        *(A2 + 0 + 4*K1) = ymm0 ;
-	//        *(A2 + 0 + 5*K1) = ymm1 ;
-	//        *(A2 + 0 + 6*K1) = ymm2 ;
-	//        *(A2 + 0 + 7*K1) = ymm3 ;
-	//        //(*A2) = (*A);
-	//        A += lda ;
-	//        A2++;
-	//      }//6
-	//    }//5
-	//    A  = A  - lda *K1 + 8;
-	//    A2 = A2 - K1 + 8*K1;
-
-	//  }//2
-	//}//1
-
 	if( m >> 2 ){
 	  size_t m4 = ( m >> 2 ); // unrolling
 	  while( m4-- ){
-	//if( m & 4 ){//7
-	//  {
 	    size_t k = K1;
-
+/*
+	    if( k >> 3 ){
+	      size_t k8 = ( k >> 3 ); // unrolling
+	      while( k8-- ){
+	        ymm0  = *(A + 0 + 0*lda); ymm4  = *(A + 0 + 1*lda); ymm8  = *(A + 0 + 2*lda); ymm12 = *(A + 0 + 3*lda);
+	        ymm1  = *(A + 1 + 0*lda); ymm5  = *(A + 1 + 1*lda); ymm9  = *(A + 1 + 2*lda); ymm13 = *(A + 1 + 3*lda);
+	        ymm2  = *(A + 2 + 0*lda); ymm6  = *(A + 2 + 1*lda); ymm10 = *(A + 2 + 2*lda); ymm14 = *(A + 2 + 3*lda);
+	        ymm3  = *(A + 3 + 0*lda); ymm7  = *(A + 3 + 1*lda); ymm11 = *(A + 3 + 2*lda); ymm15 = *(A + 3 + 3*lda);
+	        *(A2 + 0 + 0*8) = ymm0 ; *(A2 + 1 + 0*8) = ymm4 ; *(A2 + 2 + 0*8) = ymm8 ; *(A2 + 3 + 0*8) = ymm12;
+	        *(A2 + 0 + 1*8) = ymm1 ; *(A2 + 1 + 1*8) = ymm5 ; *(A2 + 2 + 1*8) = ymm9 ; *(A2 + 3 + 1*8) = ymm13;
+	        *(A2 + 0 + 2*8) = ymm2 ; *(A2 + 1 + 2*8) = ymm6 ; *(A2 + 2 + 2*8) = ymm10; *(A2 + 3 + 2*8) = ymm14;
+	        *(A2 + 0 + 3*8) = ymm3 ; *(A2 + 1 + 3*8) = ymm7 ; *(A2 + 2 + 3*8) = ymm11; *(A2 + 3 + 3*8) = ymm15;
+	        ymm0  = *(A + 0 + 4*lda); ymm4  = *(A + 0 + 5*lda); ymm8  = *(A + 0 + 6*lda); ymm12 = *(A + 0 + 7*lda);
+	        ymm1  = *(A + 1 + 4*lda); ymm5  = *(A + 1 + 5*lda); ymm9  = *(A + 1 + 6*lda); ymm13 = *(A + 1 + 7*lda);
+	        ymm2  = *(A + 2 + 4*lda); ymm6  = *(A + 2 + 5*lda); ymm10 = *(A + 2 + 6*lda); ymm14 = *(A + 2 + 7*lda);
+	        ymm3  = *(A + 3 + 4*lda); ymm7  = *(A + 3 + 5*lda); ymm11 = *(A + 3 + 6*lda); ymm15 = *(A + 3 + 7*lda);
+	        *(A2 + 4 + 0*8) = ymm0 ; *(A2 + 5 + 0*8) = ymm4 ; *(A2 + 6 + 0*8) = ymm8 ; *(A2 + 7 + 0*8) = ymm12;
+	        *(A2 + 4 + 1*8) = ymm1 ; *(A2 + 5 + 1*8) = ymm5 ; *(A2 + 6 + 1*8) = ymm9 ; *(A2 + 7 + 1*8) = ymm13;
+	        *(A2 + 4 + 2*8) = ymm2 ; *(A2 + 5 + 2*8) = ymm6 ; *(A2 + 6 + 2*8) = ymm10; *(A2 + 7 + 2*8) = ymm14;
+	        *(A2 + 4 + 3*8) = ymm3 ; *(A2 + 5 + 3*8) = ymm7 ; *(A2 + 6 + 3*8) = ymm11; *(A2 + 7 + 3*8) = ymm15;
+	        A += 8*lda ;
+	        A2+= 32;;
+	      }
+	    }
+	    if( k & 4 ){
+*/
 	    if( k >> 2 ){
 	      size_t k4 = ( k >> 2 ); // unrolling
 	      while( k4-- ){
@@ -140,39 +105,156 @@ void myblas_dgemm_copy_t_core(const double* A, size_t lda, double* A2, const blo
 	        A2+= 16;;
 	      }
 	    }
-	    if( k & 3 ){
-	      size_t kr = ( k & 3 ); 
-	      while( kr-- ){
+	    if( k & 2 ){
+	        ymm0  = *(A + 0 + 0*lda); ymm4  = *(A + 0 + 1*lda);
+	        ymm1  = *(A + 1 + 0*lda); ymm5  = *(A + 1 + 1*lda);
+	        ymm2  = *(A + 2 + 0*lda); ymm6  = *(A + 2 + 1*lda);
+	        ymm3  = *(A + 3 + 0*lda); ymm7  = *(A + 3 + 1*lda);
+	        *(A2 + 0 + 0*2) = ymm0 ; *(A2 + 1 + 0*2) = ymm4 ;
+	        *(A2 + 0 + 1*2) = ymm1 ; *(A2 + 1 + 1*2) = ymm5 ;
+	        *(A2 + 0 + 2*2) = ymm2 ; *(A2 + 1 + 2*2) = ymm6 ;
+	        *(A2 + 0 + 3*2) = ymm3 ; *(A2 + 1 + 3*2) = ymm7 ;
+	        A += 2*lda ;
+	        A2+= 8;
+	    }
+	    if( k & 1 ){
 	        ymm0  = *(A + 0 + 0*lda);
 	        ymm1  = *(A + 1 + 0*lda);
 	        ymm2  = *(A + 2 + 0*lda);
 	        ymm3  = *(A + 3 + 0*lda);
-	        *(A2 + 0 + 0*4) = ymm0 ;
-	        *(A2 + 0 + 1*4) = ymm1 ;
-	        *(A2 + 0 + 2*4) = ymm2 ;
-	        *(A2 + 0 + 3*4) = ymm3 ;
-	        //(*A2) = (*A);
+	        *(A2 + 0 + 0*1) = ymm0 ;
+	        *(A2 + 0 + 1*1) = ymm1 ;
+	        *(A2 + 0 + 2*1) = ymm2 ;
+	        *(A2 + 0 + 3*1) = ymm3 ;
 	        A += lda ;
-	        A2+=4;
+	        A2+= 4;
+	    }
+	    //if( k & 3 ){
+	    //  size_t kr = ( k & 3 ); 
+	    //  while( kr-- ){
+	    //    ymm0  = *(A + 0 + 0*lda);
+	    //    ymm1  = *(A + 1 + 0*lda);
+	    //    ymm2  = *(A + 2 + 0*lda);
+	    //    ymm3  = *(A + 3 + 0*lda);
+	    //    *(A2 + 0 + 0*4) = ymm0 ;
+	    //    *(A2 + 0 + 1*4) = ymm1 ;
+	    //    *(A2 + 0 + 2*4) = ymm2 ;
+	    //    *(A2 + 0 + 3*4) = ymm3 ;
+	    //    //(*A2) = (*A);
+	    //    A += lda ;
+	    //    A2+=4;
+	    //  }
+	    //}
+	    A  = A  - lda *K1 + 4;
+	  }
+	}
+	if( m & 2 ){
+
+	    size_t k = K1;
+/*
+	    if( k >> 3 ){
+	      size_t k8 = ( k >> 3 ); // unrolling
+	      while( k8-- ){
+	        ymm0  = *(A + 0 + 0*lda); ymm4  = *(A + 0 + 1*lda); ymm8  = *(A + 0 + 2*lda); ymm12 = *(A + 0 + 3*lda);
+	        ymm1  = *(A + 1 + 0*lda); ymm5  = *(A + 1 + 1*lda); ymm9  = *(A + 1 + 2*lda); ymm13 = *(A + 1 + 3*lda);
+	        *(A2 + 0 + 0*8) = ymm0 ; *(A2 + 1 + 0*8) = ymm4 ; *(A2 + 2 + 0*8) = ymm8 ; *(A2 + 3 + 0*8) = ymm12;
+	        *(A2 + 0 + 1*8) = ymm1 ; *(A2 + 1 + 1*8) = ymm5 ; *(A2 + 2 + 1*8) = ymm9 ; *(A2 + 3 + 1*8) = ymm13;
+	        ymm0  = *(A + 0 + 4*lda); ymm4  = *(A + 0 + 5*lda); ymm8  = *(A + 0 + 6*lda); ymm12 = *(A + 0 + 7*lda);
+	        ymm1  = *(A + 1 + 4*lda); ymm5  = *(A + 1 + 5*lda); ymm9  = *(A + 1 + 6*lda); ymm13 = *(A + 1 + 7*lda);
+	        *(A2 + 4 + 0*8) = ymm0 ; *(A2 + 5 + 0*8) = ymm4 ; *(A2 + 6 + 0*8) = ymm8 ; *(A2 + 7 + 0*8) = ymm12;
+	        *(A2 + 4 + 1*8) = ymm1 ; *(A2 + 5 + 1*8) = ymm5 ; *(A2 + 6 + 1*8) = ymm9 ; *(A2 + 7 + 1*8) = ymm13;
+	        A += 8*lda ;
+	        A2+= 16;;
 	      }
 	    }
-	    A  = A  - lda *K1 + 4;
-	    //A2 = A2 - K1 + 4*K1;
-
-	  }
-	}
-	if( m & 3 ){
-	  size_t mr = ( m & 3 );
-	  while( mr-- ){
-	    size_t k = K1;
-	    while( k-- ){
-	      (*A2) = (*A);
-	      A += lda ;
-	      A2++;
+	    if( k & 4 ){
+*/
+	    if( k >> 2 ){
+	      size_t k4 = ( k >> 2 ); // unrolling
+	      while( k4-- ){
+	        ymm0  = *(A + 0 + 0*lda); ymm4  = *(A + 0 + 1*lda); ymm8  = *(A + 0 + 2*lda); ymm12 = *(A + 0 + 3*lda);
+	        ymm1  = *(A + 1 + 0*lda); ymm5  = *(A + 1 + 1*lda); ymm9  = *(A + 1 + 2*lda); ymm13 = *(A + 1 + 3*lda);
+	        *(A2 + 0 + 0*4) = ymm0 ; *(A2 + 1 + 0*4) = ymm4 ; *(A2 + 2 + 0*4) = ymm8 ; *(A2 + 3 + 0*4) = ymm12;
+	        *(A2 + 0 + 1*4) = ymm1 ; *(A2 + 1 + 1*4) = ymm5 ; *(A2 + 2 + 1*4) = ymm9 ; *(A2 + 3 + 1*4) = ymm13;
+	        A += 4*lda ;
+	        A2+= 8;;
+	      }
 	    }
-	    A  = A  - lda *K1 + 1;
-	  }
+	    if( k & 2 ){
+	        ymm0  = *(A + 0 + 0*lda); ymm4  = *(A + 0 + 1*lda);
+	        ymm1  = *(A + 1 + 0*lda); ymm5  = *(A + 1 + 1*lda);
+	        *(A2 + 0 + 0*2) = ymm0 ; *(A2 + 1 + 0*2) = ymm4 ;
+	        *(A2 + 0 + 1*2) = ymm1 ; *(A2 + 1 + 1*2) = ymm5 ;
+	        A += 2*lda ;
+	        A2+= 4;
+	    }
+	    if( k & 1 ){
+	        ymm0  = *(A + 0 + 0*lda);
+	        ymm1  = *(A + 1 + 0*lda);
+	        *(A2 + 0 + 0*1) = ymm0 ;
+	        *(A2 + 0 + 1*1) = ymm1 ;
+	        A += lda ;
+	        A2+= 2;
+	    }
+
+	    A  = A  - lda *K1 + 2;
+
 	}
+	if( m & 1 ){
+
+	    size_t k = K1;
+/*
+	    if( k >> 3 ){
+	      size_t k8 = ( k >> 3 ); // unrolling
+	      while( k8-- ){
+	        ymm0  = *(A + 0 + 0*lda); ymm4  = *(A + 0 + 1*lda); ymm8  = *(A + 0 + 2*lda); ymm12 = *(A + 0 + 3*lda);
+	        *(A2 + 0 + 0*8) = ymm0 ; *(A2 + 1 + 0*8) = ymm4 ; *(A2 + 2 + 0*8) = ymm8 ; *(A2 + 3 + 0*8) = ymm12;
+	        ymm0  = *(A + 0 + 4*lda); ymm4  = *(A + 0 + 5*lda); ymm8  = *(A + 0 + 6*lda); ymm12 = *(A + 0 + 7*lda);
+	        *(A2 + 4 + 0*8) = ymm0 ; *(A2 + 5 + 0*8) = ymm4 ; *(A2 + 6 + 0*8) = ymm8 ; *(A2 + 7 + 0*8) = ymm12;
+	        A += 8*lda ;
+	        A2+= 8;;
+	      }
+	    }
+	    if( k & 4 ){
+*/
+	    if( k >> 2 ){
+	      size_t k4 = ( k >> 2 ); // unrolling
+	      while( k4-- ){
+	        ymm0  = *(A + 0 + 0*lda); ymm4  = *(A + 0 + 1*lda); ymm8  = *(A + 0 + 2*lda); ymm12 = *(A + 0 + 3*lda);
+	        *(A2 + 0 + 0*4) = ymm0 ; *(A2 + 1 + 0*4) = ymm4 ; *(A2 + 2 + 0*4) = ymm8 ; *(A2 + 3 + 0*4) = ymm12;
+	        A += 4*lda ;
+	        A2+= 4;;
+	      }
+	    }
+	    if( k & 2 ){
+	        ymm0  = *(A + 0 + 0*lda); ymm4  = *(A + 0 + 1*lda);
+	        *(A2 + 0 + 0*2) = ymm0 ; *(A2 + 1 + 0*2) = ymm4 ;
+	        A += 2*lda ;
+	        A2+= 2;
+	    }
+	    if( k & 1 ){
+	        ymm0  = *(A + 0 + 0*lda);
+	        *(A2 + 0 + 0*1) = ymm0 ;
+	        A += lda ;
+	        A2+= 1;
+	    }
+
+	    A  = A  - lda *K1 + 1;
+
+	}
+
+	//if( m & 3 ){
+	//  size_t mr = ( m & 3 );
+	//  while( mr-- ){
+	//    size_t k = K1;
+	//    while( k-- ){
+	//      (*A2) = (*A);
+	//      A += lda ;
+	//      A2++;
+	//    }
+	//    A  = A  - lda *K1 + 1;
+	//  }
+	//}
 
 }
 
